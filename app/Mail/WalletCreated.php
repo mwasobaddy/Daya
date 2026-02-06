d7a61817bb0ef19d487d365fb29a3f962d7d4017
Hiikey7 <kelvinramsiel@gmail.com>
2026-02-07 00:18:15 +0300

    Remove ShouldQueue from WalletCreated for synchronous sending

--- app/Mail/WalletCreated.php
+++ app/Mail/WalletCreated.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class WalletCreated extends Mailable implements ShouldQueue
+class WalletCreated extends Mailable
 {
     use Queueable, SerializesModels;
 
